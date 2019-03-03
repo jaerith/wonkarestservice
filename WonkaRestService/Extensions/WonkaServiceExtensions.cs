542ab6ad62d6097c8018a4ac7dba64345a6e1060
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-03-03 13:26:28 -0500

    Added more extension methods.

--- WonkaRestService/Extensions/WonkaServiceExtensions.cs
+++ WonkaRestService/Extensions/WonkaServiceExtensions.cs
@@ -5,11 +5,25 @@ using System.Linq;
 using System.Web;
 
 using WonkaRef;
+using WonkaPrd;
 
 namespace WonkaRestService.Extensions
 {
     public static class WonkaServiceExtensions
     {
+        public static string GetAttributeValue(this WonkaProduct poTargetProduct, WonkaRefAttr poTargetAttr)
+        {
+            if (poTargetProduct.GetProductGroup(poTargetAttr.GroupId).GetRowCount() <= 0)
+                throw new Exception("ERROR!  Provided incoming product has empty group.");
+
+            string sAttrValue = poTargetProduct.GetProductGroup(poTargetAttr.GroupId)[0][poTargetAttr.AttrId];
+
+            if (String.IsNullOrEmpty(sAttrValue))
+                throw new Exception("ERROR!  Provided incoming product has no value for needed key(" + poTargetAttr.AttrName + ").");
+
+            return sAttrValue;
+        }
+
         public static Hashtable TransformToTrxRecord(this IDictionary<string, string> poRecord)
         {
             Hashtable oRecord = new Hashtable();
@@ -65,5 +79,44 @@ namespace WonkaRestService.Extensions
 
             return oRecord;
         }
+
+        public static WonkaProduct TransformToWonkaProduct(this IDictionary<string, string> poRecord)
+        {
+            try
+            {
+                WonkaRefEnvironment.GetInstance();
+            }
+            catch (Exception ex)
+            {
+                WonkaRefEnvironment.CreateInstance(false, new WonkaRestService.WonkaData.WonkaMetadataVATSource());
+            }
+
+            WonkaRefEnvironment WonkaRefEnv = WonkaRefEnvironment.GetInstance();
+
+            WonkaProduct WonkaRecord = new WonkaProduct();
+
+            foreach (string sKeyName in poRecord.Keys)
+            {
+                WonkaRefAttr TempAttribute = WonkaRefEnv.GetAttributeByAttrName(sKeyName);
+
+                string sAttrValue = poRecord[sKeyName];
+
+                // NOTE: Test the value here
+
+                if ((TempAttribute != null) && !String.IsNullOrEmpty(sAttrValue))
+                    WonkaRecord.SetAttribute(TempAttribute, sAttrValue);
+            }
+
+            return WonkaRecord;
+        }
+
+        public static void SetAttribute(this WonkaProduct poTargetProduct, WonkaRefAttr poTargetAttr, string psTargetValue)
+        {
+            if (poTargetProduct.GetProductGroup(poTargetAttr.GroupId).GetRowCount() <= 0)
+                poTargetProduct.GetProductGroup(poTargetAttr.GroupId).AppendRow();
+
+            poTargetProduct.GetProductGroup(poTargetAttr.GroupId)[0][poTargetAttr.AttrId] = psTargetValue;
+        }
+
     }
 }
