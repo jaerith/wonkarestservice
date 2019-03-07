202037e0f44e015495c378655aa988d7c72bcd7f
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-03-07 13:38:19 -0500

    Added singleton that will maintain a cache holding instances of the rules engine.

---
+++ WonkaRestService/Cache/WonkaServiceCache.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+using WonkaBre;
+
+namespace WonkaRestService.Cache
+{
+    /// <summary>
+    /// 
+    /// This singleton, when initialized, will contain all of the cached data
+    /// that drives the Wonka service.
+    /// 
+    /// </summary>
+    [DataContract(Namespace = "http://wonkarestservice.com")]
+    public class WonkaServiceCache
+    {
+        private static object mLock = new object();
+
+        private static WonkaServiceCache mInstance = null;
+
+        // NOTE: This constructor is necessary for serialization/deserialization purposes
+        private WonkaServiceCache()
+        {
+            RuleTreeCache = new Dictionary<string, WonkaBreRulesEngine>();
+        }
+
+        static public WonkaServiceCache CreateInstance()
+        {
+            lock (mLock)
+            {
+                if (mInstance == null)
+                    mInstance = new WonkaServiceCache();
+
+                return mInstance;
+            }
+        }
+
+        static public WonkaServiceCache GetInstance()
+        {
+            lock (mLock)
+            {
+                if (mInstance == null)
+                    throw new Exception("ERROR!  WonkaServiceCache has not yet been initialized!");
+
+                return mInstance;
+            }
+        }
+
+        #region Methods
+
+        #endregion
+
+        #region Properties 
+
+        public Dictionary<string, WonkaBreRulesEngine> RuleTreeCache { get; set; }
+
+        #endregion
+    }
+}
+
