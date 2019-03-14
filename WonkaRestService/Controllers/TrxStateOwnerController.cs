21150c882f7afc4a1f21c23c479b664060ead0c9
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-03-14 11:48:40 -0400

    Finished initial implementation of TrxStateOwnerController.

--- WonkaRestService/Controllers/TrxStateOwnerController.cs
+++ WonkaRestService/Controllers/TrxStateOwnerController.cs
@@ -16,18 +16,19 @@ namespace WonkaRestService.Controllers
     {
         /// <summary>
         /// 
-        /// This method will return the trx state of a RuleTree identified by the RuleTreeId.
+        /// This method will return the Trx State Owner of a RuleTree identified by the RuleTreeId.
         /// 
         /// GET: api/TrxState/RuleTreeId/Owner
         /// 
-        /// <param name="psRuleTreeId">The ID of the RuleTree</param>
-        /// <returns>Contains the Response with the trx state (or an error message if an error occurs)</returns>
+        /// <param name="RuleTreeId">The ID of the RuleTree</param>
+        /// <param name="Owner">The ID of the Owner</param>
+        /// <returns>Contains the Response with the trx state owner (or an error message if an error occurs)</returns>
         /// </summary>
-        public HttpResponseMessage Get(string RuleTreeId, string Owner)
+        public HttpResponseMessage GetTrxStateOwner(string RuleTreeId, string Owner)
         {
             SvcTrxStateOwner TrxStateOwner = new SvcTrxStateOwner("", false, 0);
 
-            var response = Request.CreateResponse<SvcTrxStateOwner>(HttpStatusCode.Created, TrxStateOwner);
+            var response = Request.CreateResponse<SvcTrxStateOwner>(HttpStatusCode.OK, TrxStateOwner);
 
             string uri = Url.Link("DefaultApi", new { id = "DefaultValue" });
 
@@ -67,7 +68,7 @@ namespace WonkaRestService.Controllers
             }
             catch (Exception ex)
             {
-                string sErrorMsg = String.Format("ERROR!  Trx State web method -> Error Message : {0}",
+                string sErrorMsg = String.Format("ERROR!  Trx State Owner web method -> Error Message : {0}",
                                                  ex.ToString());
 
                 if ((ex.InnerException != null) && (ex.InnerException.Message != null))
@@ -83,19 +84,143 @@ namespace WonkaRestService.Controllers
             return response;
         }
 
-        // POST: api/TrxStateOwner
-        public void Post([FromBody]string value)
+        /// <summary>
+        /// 
+        /// This method will set the Trx State Owner of a RuleTree identified by the RuleTreeId.
+        /// 
+        /// POST: api/TrxStateOwner
+        /// 
+        /// TEST PAYLOAD:
+        /// {
+        ///   "RuleTreeId": 
+        ///   "OwnerName": "Dummy",
+        ///   "ConfirmedTransaction": true,
+        ///   "OwnerWeight": 1
+        /// }
+        /// 
+        /// <param name="TrxStateOwner">The Owner whose information will be updated for the RuleTree (i.e., the engine instance)</param>
+        /// <returns>Contains the Response with the trx state owner (or an error message if an error occurs)</returns>
+        /// </summary>
+        public HttpResponseMessage PostTrxStateOwner(SvcTrxStateOwner TrxStateOwner)
         {
+            var response = Request.CreateResponse<SvcTrxStateOwner>(HttpStatusCode.Created, TrxStateOwner);
+
+            string uri = Url.Link("DefaultApi", new { id = "DefaultValue" });
+
+            response.Headers.Location = new Uri(uri);
+
+            try
+            {
+                if (TrxStateOwner == null)
+                    throw new Exception("ERROR!  No transaction state was provided.");
+
+                if (TrxStateOwner.IsValid())
+                    throw new Exception("ERROR!  Invalid transaction state was provided.");
+
+                string sTargetRuleTreeId = TrxStateOwner.RuleTreeId;
+
+                WonkaServiceCache ServiceCache = WonkaServiceCache.CreateInstance();
+
+                WonkaBreRulesEngine RulesEngine = null;
+                if (ServiceCache.RuleTreeCache.ContainsKey(sTargetRuleTreeId))
+                {
+                    RulesEngine = ServiceCache.RuleTreeCache[sTargetRuleTreeId];
+
+                    if (RulesEngine.TransactionState != null)
+                    {
+                        RulesEngine.TransactionState.SetOwner(TrxStateOwner.OwnerName, TrxStateOwner.OwnerWeight);
+
+                        if (TrxStateOwner.ConfirmedTransaction)
+                            RulesEngine.TransactionState.AddConfirmation(TrxStateOwner.OwnerName);
+                    }
+                }
+
+                response = Request.CreateResponse<SvcTrxStateOwner>(HttpStatusCode.Created, TrxStateOwner);
+            }
+            catch (Exception ex)
+            {
+                string sErrorMsg = String.Format("ERROR!  Trx State Owner web method -> Error Message : {0}",
+                                                 ex.ToString());
+
+                if ((ex.InnerException != null) && (ex.InnerException.Message != null))
+                    TrxStateOwner.ErrorMessage = ex.InnerException.Message;
+                else if (!String.IsNullOrEmpty(ex.Message))
+                    TrxStateOwner.ErrorMessage = ex.Message;
+
+                response = Request.CreateResponse<SvcTrxStateOwner>(HttpStatusCode.BadRequest, TrxStateOwner);
+
+                Elmah.ErrorSignal.FromCurrentContext().Raise(ex);
+            }
+
+            return response;
         }
 
+        /*
         // PUT: api/TrxStateOwner/5
         public void Put(int id, [FromBody]string value)
         {
         }
+         */
 
-        // DELETE: api/TrxStateOwner/5
-        public void Delete(int id)
+        /// <summary>
+        /// 
+        /// This method will delete the Trx State Owner of a RuleTree identified by the RuleTreeId.
+        /// 
+        /// DELETE: api/TrxState/RuleTreeId/Owner
+        /// 
+        /// <param name="RuleTreeId">The ID of the RuleTree</param>
+        /// <param name="Owner">The ID of the Owner</param>
+        /// <returns>Contains the Response with the trx state owner (or an error message if an error occurs)</returns>
+        /// </summary>        
+        public HttpResponseMessage DeleteTrxStateOwner(string RuleTreeId, string Owner)
         {
+            SvcTrxStateOwner TrxStateOwner = new SvcTrxStateOwner("", false, 0);
+
+            var response = Request.CreateResponse<SvcTrxStateOwner>(HttpStatusCode.Created, TrxStateOwner);
+
+            string uri = Url.Link("DefaultApi", new { id = "DefaultValue" });
+
+            response.Headers.Location = new Uri(uri);
+
+            try
+            {
+                string sTargetRuleTreeId =
+                    (!String.IsNullOrEmpty(RuleTreeId)) ? RuleTreeId : InvokeController.CONST_RULES_RESOURCE_STREAM;
+
+                WonkaServiceCache ServiceCache = WonkaServiceCache.CreateInstance();
+
+                WonkaBreRulesEngine RulesEngine = null;
+                if (ServiceCache.RuleTreeCache.ContainsKey(sTargetRuleTreeId))
+                {
+                    RulesEngine = ServiceCache.RuleTreeCache[sTargetRuleTreeId];
+
+                    if ((RulesEngine.TransactionState != null) && (RulesEngine.TransactionState is WonkaBre.Permissions.WonkaBreTransactionState))
+                    {
+                        if (RulesEngine.TransactionState.IsOwner(Owner))
+                            RulesEngine.TransactionState.RemoveOwner(Owner);
+                        else
+                            throw new Exception("ERROR!  Not a registered owner of this RuleTree.");
+                    }
+                }
+
+                response = Request.CreateResponse<SvcTrxStateOwner>(HttpStatusCode.Created, TrxStateOwner);
+            }
+            catch (Exception ex)
+            {
+                string sErrorMsg = String.Format("ERROR!  Trx State Owner web method -> Error Message : {0}",
+                                                 ex.ToString());
+
+                if ((ex.InnerException != null) && (ex.InnerException.Message != null))
+                    TrxStateOwner.ErrorMessage = ex.InnerException.Message;
+                else if (!String.IsNullOrEmpty(ex.Message))
+                    TrxStateOwner.ErrorMessage = ex.Message;
+
+                response = Request.CreateResponse<SvcTrxStateOwner>(HttpStatusCode.BadRequest, TrxStateOwner);
+
+                Elmah.ErrorSignal.FromCurrentContext().Raise(ex);
+            }
+
+            return response;
         }
     }
 }
