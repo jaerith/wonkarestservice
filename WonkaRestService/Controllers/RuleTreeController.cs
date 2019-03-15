3b1592de6e2e3a34feba16c06903ec8174a7ba95
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-03-15 17:57:12 -0400

    Added properties that expose collections of WonkaBreSources inside a RuleTree.

--- WonkaRestService/Controllers/RuleTreeController.cs
+++ WonkaRestService/Controllers/RuleTreeController.cs
@@ -5,20 +5,69 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+using WonkaBre;
+
+using WonkaRestService.Cache;
+using WonkaRestService.Models;
+
 namespace WonkaRestService.Controllers
 {
     public class RuleTreeController : ApiController
     {
-        // GET: api/RuleTree
-        public IEnumerable<string> Get()
+        /// <summary>
+        /// 
+        /// This method will return information about a RuleTree, if it exists within the cache.
+        /// 
+        /// GET: api/RuleTree/RuleTreeId
+        /// 
+        /// <param name="RuleTreeId">The ID of the RuleTree</param>
+        /// <returns>Contains the Response with the RuleTree (or an error message if an error occurs)</returns>
+        /// </summary>
+        public HttpResponseMessage GetRuleTree(string RuleTreeId)
         {
-            return new string[] { "value1", "value2" };
-        }
+            SvcRuleTree RuleTree = new SvcRuleTree();
 
-        // GET: api/RuleTree/5
-        public string Get(int id)
-        {
-            return "value";
+            var response = Request.CreateResponse<SvcRuleTree>(HttpStatusCode.OK, RuleTree);
+
+            string uri = Url.Link("DefaultApi", new { id = "DefaultValue" });
+
+            response.Headers.Location = new Uri(uri);
+
+            try
+            {
+                string sTargetRuleTreeId = 
+                    (!String.IsNullOrEmpty(RuleTreeId)) ? RuleTreeId : InvokeController.CONST_RULES_RESOURCE_STREAM;
+
+                WonkaServiceCache ServiceCache = WonkaServiceCache.CreateInstance();
+
+                WonkaBreRulesEngine RulesEngine = null;
+                if (ServiceCache.RuleTreeCache.ContainsKey(sTargetRuleTreeId))
+                {
+                    RuleTree.RuleTreeId = RuleTreeId;
+
+                    RulesEngine = ServiceCache.RuleTreeCache[sTargetRuleTreeId];
+                    if (RulesEngine != null)
+                        RuleTree.RulesEngine = RulesEngine;
+                }
+
+                response = Request.CreateResponse<SvcRuleTree>(HttpStatusCode.Created, RuleTree);
+            }
+            catch (Exception ex)
+            {
+                string sErrorMsg = String.Format("ERROR!  Rules Engine web method -> Error Message : {0}",
+                                                 ex.ToString());
+
+                if ((ex.InnerException != null) && (ex.InnerException.Message != null))
+                    RuleTree.ErrorMessage = ex.InnerException.Message;
+                else if (!String.IsNullOrEmpty(ex.Message))
+                    RuleTree.ErrorMessage = ex.Message;
+
+                response = Request.CreateResponse<SvcRuleTree>(HttpStatusCode.BadRequest, RuleTree);
+
+                Elmah.ErrorSignal.FromCurrentContext().Raise(ex);
+            }
+
+            return response;
         }
 
         // POST: api/RuleTree
