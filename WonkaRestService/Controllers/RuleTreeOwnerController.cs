fa4364fdaaee7fc10ec3b48fa3d333b79668589f
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-03-22 15:50:51 -0400

    Initial revision of the SvcRuleTreeOwner and RuleTreeOwnerController classes.

---
+++ WonkaRestService/Controllers/RuleTreeOwnerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WonkaRestService.Controllers
+{
+    public class RuleTreeOwnerController : ApiController
+    {
+        // GET: api/RuleTreeOwner
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/RuleTreeOwner/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/RuleTreeOwner
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/RuleTreeOwner/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/RuleTreeOwner/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WonkaRestService/Models/SvcRuleTreeOwner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace WonkaRestService.Models
+{
+    [DataContract(Namespace = "http://wonkarestservice.com")]
+    public class SvcRuleTreeOwner
+    {
+        public SvcRuleTreeOwner(string psOwnerName, string psAddress, string psPassword)
+        {
+            OwnerName = psOwnerName;
+
+            OwnerAddress  = psAddress;
+            OwnerPassword = psPassword;
+        }
+
+        #region Properties
+
+        [DataMember, XmlElement(IsNullable = false), JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string OwnerName { get; set; }
+
+        [DataMember, XmlElement(IsNullable = false), JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string OwnerAddress { get; set; }
+
+        [DataMember, XmlElement(IsNullable = false), JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string OwnerPassword { get; set; }
+
+        [DataMember, XmlElement(IsNullable = false), JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string ErrorMessage { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public bool IsValid()
+        {
+            bool bIsValid = true;
+
+            if (String.IsNullOrEmpty(OwnerName))
+                bIsValid = false;
+
+            if (String.IsNullOrEmpty(OwnerAddress))
+                bIsValid = false;
+
+            if (String.IsNullOrEmpty(OwnerPassword))
+                bIsValid = false;
+
+            return bIsValid;
+        }
+
+        #endregion
+    }
+}
